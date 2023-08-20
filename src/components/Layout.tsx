603614b0a6bd8c5e30405e7f0b17d85f6eb6ee0e
erfan004 <erfanbob1384@gmail.com>
2023-08-20 12:20:43 +0330

    style: :lipstick: changing navigate to note page with icon
    changing navigate to note page with icon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import { HomeIcon } from '@heroicons/react/24/solid'
 const Layout = ({children}: {children : React.ReactNode}) => {
   return (
     <div>
@@ -9,10 +10,9 @@ const Layout = ({children}: {children : React.ReactNode}) => {
         </div>
       </Link>
       <Link to={"/"}>
-        <div className="text-2xl cursor-pointer capitalize  bg-zinc-600 fixed hover:bg-zinc-700 hover:text-white duration-300 pt-0 bottom-3 text-gray-400  px-3 py-2 left-5 rounded-full">
-          show notes
-        </div>
+        <HomeIcon className="w-16 cursor-pointer capitalize  bg-zinc-600 fixed hover:bg-zinc-700 hover:text-white duration-300 bottom-3 text-gray-400  px-3 py-2 left-5 rounded-full" />
       </Link>
+      
     </div>
   );
 };
